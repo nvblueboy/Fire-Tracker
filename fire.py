91cae4fe6f0fd95fcb47c0ec295fa5b6deee354e
Dylan Bowman <dbowman06@gmail.com>
2020-09-07 16:15:02 -0700

    First Commit

---
+++ fire.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+class Fire:
+    def __init__(self):
+        self.title = ""
+        self.published = ""
+        self.pubdate = ""
+        self.lat = 0
+        self.long = 0
+        self.link = ""
+        self.guid = ""
+        self.description = ""
+    
+    def __str__(self):
+        output = ""
+        output += "title:" + self.title + "\n"
+        output += "published:" + self.published + "\n"
+        output += "pubdate:" + datetime.strftime(self.pubdate, '%d/%m/%Y') + "\n"
+        output += "lat:" + str(self.lat) + "\n"
+        output += "long:" + str(self.long) + "\n"
+        output += "link:" + self.link + "\n"
+        output += "guid:" + self.guid + "\n"
+        output += "description:" + self.description + "\n"
+        return output
+        
+    def formatAsHtml(self):
+        output = ""
+
+        #Format title as link
+        output += '<a href="' + self.link + '">'
+        output += self.title
+        output += "</a>" + "\n"
+
+        #add description
+        if self.description != "":
+            output += "<br/>\n"
+            output += self.description
+        else:
+            output += "<br/>\nNo Description."
+
+        return output
---
+++ inciweb.py
@@ -0,0 +1,63 @@
+import requests
+import xml.etree.ElementTree as ET
+from fire import Fire
+from datetime import datetime, timedelta
+
+namespaces = {"geo" : "http://www.w3.org/2003/01/geo/wgs84_pos#"}
+pubDateFormat = '%d %b %Y'
+
+def getRSSData():
+    url = "https://inciweb.nwcg.gov/feeds/rss/incidents/"
+
+    r = requests.get(url)
+
+    if r.status_code == 200:
+        return r.text
+    else:
+        return None
+
+def parseXML(rssData):
+    root = ET.fromstring(rssData)
+
+    output = []
+
+    for child in root.iter("item"):
+        f = Fire()
+        link = child.find('link')
+        if link != None:
+            f.link = link.text
+
+        title = child.find('title')
+        if  title != None:
+            f.title = title.text
+
+        lon = child.find('geo:long', namespaces)
+        if lon != None:
+            f.long = float(lon.text)
+
+        lat = child.find('geo:lat', namespaces)
+        if lat != None:
+            f.lat = float(lat.text)
+
+        guid = child.find('guid')
+        if guid != None:
+            f.guid = guid.text
+
+        description = child.find('description')
+        if description != None:
+            f.description = description.text
+
+        pubDate = child.find('pubDate')
+        if pubDate != None:
+            f.pubdate = datetime.strptime(pubDate.text.split(", ")[1][:11], pubDateFormat)
+
+        output.append(f)
+
+    return output
+
+if __name__ == "__main__":
+    rssData = getRSSData()
+
+    for fire in parseXML(rssData)[:5]:
+        print(fire.formatAsHtml())
+        print(fire)
